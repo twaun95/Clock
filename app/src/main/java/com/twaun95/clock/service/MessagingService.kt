1fcf6b9aad54e25621bd97da693195d99e67bebd
taewaun <taewaunkwon@gmail.com>
2022-09-15 23:27:30 +0900

    FCM(Firebase Cloud Messaging) 세팅

---
+++ app/src/main/java/com/twaun95/clock/service/MessagingService.kt
@@ -0,0 +1,25 @@
+package com.twaun95.clock.service
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.twaun95.clock.common.Logger
+
+class MessagingService : FirebaseMessagingService(){
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+
+        Logger.d("onNewToken, token: $token")
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        Logger.d("onMessageReceived, message: $message")
+    }
+
+
+
+    fun sendNotification() {
+
+    }
+}
